71c98588f4dff60ad0251dcdf6c87dc7b3264d9c
Ryan Archer <ryanarcheryeah@gmail.com>
2023-09-15 00:50:04 -0400

    Adding Requirements as comments

--- deck.js
+++ deck.js
@@ -1,2 +1,8 @@
+// Create an object called deck of cards.
+//This deck of cards needs to a method for creating a new deck of cards
+//Another method to shuffle
+//And a method to draw a hand of n number cards.  .pop()
+//If you create a new deck, it will restart the deck
+
 let cardSuits = ['J','Q','K','A']
 let cardNumbers = ['1','2','3','4','5','6','7','8','9']
